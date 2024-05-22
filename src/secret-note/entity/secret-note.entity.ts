45cc4d2928d853ad91a189b516f3ef850559e7bb
YoshinoriHatori <yoshinorihatori0924@gmail.com>
2024-05-22 13:40:24 +0300

    add dto and entity for interaction with db

---
+++ src/secret-note/entity/secret-note.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class SecretNote {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text')
+  note: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
